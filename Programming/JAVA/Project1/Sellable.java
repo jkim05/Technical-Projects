79bdc5af4756ce0607ba098fca2abbec5b7cda5f
Jefferson K <121065503+jkim05@users.noreply.github.com>
2025-07-14 07:58:24 -0400

    Add files via upload

---
+++ Programming/JAVA/Project1/Sellable.java
@@ -0,0 +1,16 @@
+/*
+//*********************************************************
+/*
+   Name: Jefferson T. Kim
+   Date: 04/11/2022
+   Course/Section: IT 206.2D1
+*/
+public interface Sellable {
+    /**
+     * Returns the cost of the sellable item.
+     *
+     * @return The cost as a double.
+     */
+    public double getCost();
+}
+//*********************************************************
